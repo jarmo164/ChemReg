43f2a968c4221d17d64a62a615cf17094bf368e6
marleenkirsipuu <marleen@bauhub.ee>
2026-04-12 19:19:27 +0300

    feature/login: fix base url

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,4 +1,8 @@
 // API Configuration - default tenant uuid for mvp purposes
 export const DEFAULT_TENANT_ID = '3fa85f64-5717-4562-b3fc-2c963f66afa6';
 
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+if (!process.env.REACT_APP_API_URL) {
+  throw new Error("REACT_APP_API_URL is not defined");
+}
+
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
