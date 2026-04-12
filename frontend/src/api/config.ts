8564e1979725bb7f8bd36d15309ec4e53e663c41
marleenkirsipuu <marleen@bauhub.ee>
2026-04-12 20:21:39 +0300

    feature/login: update hardcoded tenant id

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,5 +1,5 @@
 // API Configuration - default tenant uuid for mvp purposes
-export const DEFAULT_TENANT_ID = '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+export const DEFAULT_TENANT_ID =  '11111111-1111-1111-1111-111111111111';
 
 if (!process.env.REACT_APP_API_URL) {
   throw new Error("REACT_APP_API_URL is not defined");
